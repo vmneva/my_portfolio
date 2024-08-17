dde40c732c82c845e0f31768f628e9bb95e8667f
Viivi Nevalainen <viivi.99@windowslive.com>
2024-08-17 20:04:06 +0300

    Add responsiveness to mainpage. Add hamburger menu for mobile.

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,35 +1,15 @@
 // Select DOM Items
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-item');
+const toggleBtn = document.querySelector('.toggle_btn');
+const toggleIcon = document.querySelector('.toggle_btn i');
+const dropDown = document.querySelector('.dropdown_menu');
 
-// Set Initial State Of Menu
-let showMenu = false;
-
-menuBtn.addEventListener('click', toggleMenu);
+toggleBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-    if(!showMenu) {
-        menuBtn.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        menuBranding.classList.add('show');
-        navItems.forEach(item => item.classList.add('show'));
+  dropDown.classList.toggle('open');
+  const isOpen = dropDown.classList.contains('open');
 
-        // Set Menu State
-        showMenu = true;
-    } else {
-        menuBtn.classList.remove('close');
-        menu.classList.remove('show');
-        menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
-        navItems.forEach(item => item.classList.remove('show'));
-
-        // Set Menu State
-        showMenu = false;
-    }
+  toggleIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -39,9 +19,10 @@ document.addEventListener('DOMContentLoaded', () => {
       accordion.addEventListener('click', () => {
         // Close all accordions
         accordions.forEach(item => {
+          const content = item.nextElementSibling;
           if (item !== accordion) {
             item.classList.remove('active');
-            item.nextElementSibling.style.maxHeight = 0;
+            content.style.maxHeight = 0;
           }
         });
   
